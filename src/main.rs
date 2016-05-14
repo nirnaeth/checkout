2739088ad88bc2c52b0d52620e451f1f223bf5b1
Monica Giambitto <monica.giambitto@gmail.com>
2016-05-14 13:48:40 +0200

    introduce discounts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(slice_patterns)]
+#![feature(slice_patterns, advanced_slice_patterns)]
 type Product = u32;
 
 const PRODUCT_A: u32 = 50;
@@ -9,36 +9,54 @@ fn price(product: u32) -> u32 { product }
 
 fn empty_basket() -> Vec<Product> { vec!() }
 
-fn total_price(basket: &[Product]) -> u32 {
+fn sorted(basket: Vec<Product>) -> Vec<Product> {
+    let mut tmp = basket.clone();
+    tmp.sort();
+    return tmp;
+}
+
+fn sum_price(basket: &[Product]) -> u32 {
     match basket {
         [] => 0,
-        [x, rest..] => price(x) + total_price(rest)
+        [PRODUCT_A, PRODUCT_A, PRODUCT_A, rest..] => 130 + sum_price(rest),
+        [PRODUCT_B, PRODUCT_B, rest..] => 45 + sum_price(rest),
+        [x, rest..] => price(x) + sum_price(rest)
     }
 }
 
+fn total_price(basket: Vec<Product>) -> u32 {
+    sum_price(sorted(basket).as_slice())
+}
 
 #[test]
 fn total_price_of_an_empty_basket_is_0() {
-    assert_eq!(total_price(empty_basket().as_slice()), 0);
+    assert_eq!(total_price(empty_basket()), 0);
 }
 
 #[test]
 fn total_price_of_a_basket_with_a_product_is_the_price_of_the_product() {
     let basket = vec!(PRODUCT_A);
 
-    assert_eq!(total_price(basket.as_slice()), PRODUCT_A);
+    assert_eq!(total_price(basket), PRODUCT_A);
 }
 
 #[test]
 fn total_price_of_a_basket_with_two_products_is_their_sum() {
     let basket = vec!(PRODUCT_B, PRODUCT_C);
 
-    assert_eq!(total_price(basket.as_slice()), 50);
+    assert_eq!(total_price(basket), 50);
 }
 
 #[test]
-fn total_price_of_a_basket_with_offer_products_is_discounted() {
-    let basket = vec!(PRODUCT_A, PRODUCT_A, PRODUCT_A, PRODUCT_B);
+fn total_price_of_a_basket_with_offer_products_a_is_discounted() {
+    let basket = vec!(PRODUCT_A, PRODUCT_B, PRODUCT_A, PRODUCT_A);
 
-    assert_eq!(total_price(basket.as_slice()), 160);
+    assert_eq!(total_price(basket), 160);
+}
+
+#[test]
+fn total_price_of_a_basket_with_offer_products_b_is_discounted() {
+    let basket = vec!(PRODUCT_A, PRODUCT_B, PRODUCT_A, PRODUCT_B);
+
+    assert_eq!(total_price(basket), 145);
 }
